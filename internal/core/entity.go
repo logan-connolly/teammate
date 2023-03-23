fd904a322c6f7d147b53a7d73bb9ce9f502386ba
Logan Connolly <logan.connolly@protonmail.com>
2023-03-23 17:40:12 +0100

    feat(core): add Person root entity

---
+++ internal/core/entity.go
@@ -0,0 +1,13 @@
+package core
+
+import (
+	"github.com/google/uuid"
+)
+
+// Person is a entity that represents a person in all Domains
+type Person struct {
+	// ID is the identifier of the Entity, the ID is shared for all sub domains
+	ID uuid.UUID
+	// Name is the name of the person
+	Name string
+}
