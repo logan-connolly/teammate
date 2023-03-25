370e77c9721637d5623ae848ecb80324baac9d0f
Logan Connolly <logan.connolly@protonmail.com>
2023-03-25 13:50:53 +0100

    feat(services): test player registration configs

--- internal/services/registration/registration_test.go
+++ internal/services/registration/registration_test.go
@@ -9,6 +9,40 @@ import (
 
 const exampleName = "Matt"
 
+func TestNewRegistrationService(t *testing.T) {
+	ErrInvalidConfig := errors.New("Invalid configuration")
+
+	type testCase struct {
+		test        string
+		testConfig  func() RegistrationConfiguration
+		expectedErr error
+	}
+
+	testCases := []testCase{
+		{
+			test:        "With in-memory repository",
+			testConfig:  WithMemoryPlayerRepository,
+			expectedErr: nil,
+		},
+		{
+			test: "With bad config",
+			testConfig: func() RegistrationConfiguration {
+				return func(s *RegistrationService) error {
+					return ErrInvalidConfig
+				}
+			},
+			expectedErr: ErrInvalidConfig,
+		},
+	}
+
+	for _, tc := range testCases {
+		_, err := NewRegistrationService(tc.testConfig())
+		if !errors.Is(err, tc.expectedErr) {
+			t.Errorf("Expected %v, but got %v.", tc.expectedErr, err)
+		}
+	}
+}
+
 func TestRegistrationService_RegisterPlayer(t *testing.T) {
 	type testCase struct {
 		test         string
