eb6a25151e2d21f01320fa7588cf312a6617b62b
Logan Connolly <logan.connolly@protonmail.com>
2023-03-30 20:32:26 +0200

    feat(team): define GetTeams to player repository

--- internal/team/domain/repository/player.go
+++ internal/team/domain/repository/player.go
@@ -16,6 +16,7 @@ var (
 // PlayerRepository defines the interface for the player repository.
 type PlayerRepository interface {
 	Get(*entity.Person) (*model.Player, error)
+	GetTeams(*entity.Person) ([]*entity.Group, error)
 	Add(*model.Player) error
 	Update(*model.Player) error
 }
